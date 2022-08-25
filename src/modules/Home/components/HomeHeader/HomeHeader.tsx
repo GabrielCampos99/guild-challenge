48da8ec8b805a34404cf61448cc13fc77c1b25d2
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2022-08-24 22:47:53 -0300

    feat: home component

---
+++ src/modules/Home/components/HomeHeader/HomeHeader.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import * as S from './styles'
+
+type THomeContent = {}
+
+const HomeContent: React.FC<THomeContent> = () => {
+  return (
+    <S.Wrapper>
+      <S.Content>
+        <S.Header>Welcome to the Trivia Challenge</S.Header>
+        <S.Text>You will be presented with 10 True or False questions.</S.Text>
+        <S.Text>Can you Score 100%?</S.Text>
+        <S.Button>Begin</S.Button>
+      </S.Content>
+    </S.Wrapper>
+  )
+}
+
+export default HomeContent
