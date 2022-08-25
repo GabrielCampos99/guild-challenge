04f3a5740b71e424d4aa1bcaefa8f5517d1b23f9
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2022-08-24 22:48:00 -0300

    feat: home view

--- src/modules/Home/views/Home.tsx
+++ src/modules/Home/views/Home.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import HomeContent from '../components/HomeHeader/HomeHeader'
 
 type HomeProps = {}
 
 const Home: React.FC<HomeProps> = () => {
   return (
-    <div>HoTESTEme</div>
+    <div>
+      <HomeContent />
+    </div>
   )
 }
 
