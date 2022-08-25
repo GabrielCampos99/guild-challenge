9a20bdfa5d224cbec37c65eaa5112d5b0efaed45
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2022-08-24 22:52:28 -0300

    refactor: remove unused div

--- src/modules/Home/views/Home.tsx
+++ src/modules/Home/views/Home.tsx
@@ -4,11 +4,7 @@ import HomeContent from '../components/HomeHeader/HomeHeader'
 type HomeProps = {}
 
 const Home: React.FC<HomeProps> = () => {
-  return (
-    <div>
-      <HomeContent />
-    </div>
-  )
+  return <HomeContent />
 }
 
 export default Home
