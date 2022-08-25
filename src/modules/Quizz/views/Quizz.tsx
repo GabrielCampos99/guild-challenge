ef6a46d15686c7a10980920e9e9e6647b6ddeb16
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2022-08-24 23:06:11 -0300

    feat: quizz view

---
+++ src/modules/Quizz/views/Quizz.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import QuizzContent from '../components/QuizzContent/QuizzContent'
+
+type TQuizz = {}
+
+const Quizz: React.FC<TQuizz> = () => {
+  return <QuizzContent />
+}
+
+export default Quizz
